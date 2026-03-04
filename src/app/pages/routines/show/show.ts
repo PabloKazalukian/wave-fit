92a3260967625d5c794d4375d220d9452404306a
Pablo Kaza <kazalukianpablo@gmail.com>
2026-03-04 20:27:18 -0300

    feature(show) add new info to show it

--- src/app/pages/routines/show/show.ts
+++ src/app/pages/routines/show/show.ts
@@ -1,15 +1,17 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlansService } from '../../../core/services/plans/plans.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RoutineDay, RoutinePlanCreate } from '../../../shared/interfaces/routines.interface';
 import { ExerciseCategory } from '../../../shared/interfaces/exercise.interface';
 import { AccordionItemComponent } from '../../../shared/components/ui/accordion-item/accordion-item';
 import { ExerciseCategoryPipe } from '../../../shared/pipes/exercise-category.pipe';
+import { BtnComponent } from '../../../shared/components/ui/btn/btn';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-show',
-    imports: [AccordionItemComponent, ExerciseCategoryPipe],
+    imports: [AccordionItemComponent, ExerciseCategoryPipe, BtnComponent, NgClass],
     standalone: true,
     templateUrl: './show.html',
     styles: ``,
@@ -22,30 +24,28 @@ export class Show implements OnInit {
     isSelected = signal<boolean | null>(null);
     routinesDays = signal<RoutineDay[]>([]);
 
-    private openAccordionIndex = signal<number | null>(0);
+    isStartingRoutine = signal<boolean>(false);
 
+    private openAccordionIndex = signal<number | null>(0);
     openIndex = signal<number | null>(null);
 
     private readonly route = inject(ActivatedRoute);
+    private readonly router = inject(Router);
     private readonly svcRoutines = inject(PlansService);
 
     exerciseCategory = ExerciseCategory;
 
     ngOnInit() {
-        // Subscribe to changes in route parameters (for dynamic updates)
         this.route.params.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((params) => {
             this.userId.set(params['id']);
-            console.log('User ID:', this.userId()); // '123' or 'john-doe'
             if (this.userId() !== '' || this.userId() !== undefined || this.userId() !== null) {
                 this.svcRoutines
                     .getRoutinePlanById(this.userId())
                     .pipe(takeUntilDestroyed(this.destroyRef))
                     .subscribe((result: RoutinePlanCreate | null) => {
-                        console.log('Routine Plan:', result);
                         this.routinePlan.set(result);
                     });
             }
-            // Use the userId to fetch data for that specific user
         });
     }
 
@@ -55,5 +55,27 @@ export class Show implements OnInit {
 
     toggleAccordion(index: number): void {
         this.openAccordionIndex.set(this.openAccordionIndex() === index ? null : index);
+    }
+
+    initTrackingWithRoutinePlan(): void {
+        const plan = this.routinePlan();
+        if (!plan) return;
+
+        this.isStartingRoutine.set(true);
+
+        //     this.svcRoutines
+        //         .initWeeklyTracking(plan)
+        //         .pipe(takeUntilDestroyed(this.destroyRef))
+        //         .subscribe({
+        //             next: (result) => {
+        //                 this.isStartingRoutine.set(false);
+        //                 // Navegar al tracking creado o a la pantalla de rutina activa
+        //                 this.router.navigate(['/tracking', result?.id]);
+        //             },
+        //             error: (err) => {
+        //                 console.error('Error iniciando rutina semanal:', err);
+        //                 this.isStartingRoutine.set(false);
+        //             },
+        //         });
     }
 }
