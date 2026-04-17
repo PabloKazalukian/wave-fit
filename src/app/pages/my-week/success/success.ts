91feae52402e3c85423b39e5966b4ec8ea6bf92f
Pablo Kaza <kazalukianpablo@gmail.com>
2026-04-17 11:04:34 -0300

    add my-week/success page

--- src/app/pages/my-week/success/success.ts
+++ src/app/pages/my-week/success/success.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from '../../../shared/components/ui/btn/btn';
 import { WaveLogoTextComponent } from '../../../shared/components/ui/logos/wave-logo-text/wave-logo-text';
+import { WeeklyTrackings } from '../../../shared/components/widgets/users/weekly-trackings/weekly-trackings';
 
 @Component({
     selector: 'app-success',
-    imports: [BtnComponent, WaveLogoTextComponent],
+    imports: [BtnComponent, WaveLogoTextComponent, WeeklyTrackings],
     standalone: true,
     templateUrl: './success.html',
     styles: ``,
