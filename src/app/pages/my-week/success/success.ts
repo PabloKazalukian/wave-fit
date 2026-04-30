a253c980f35561ccdfcd9f01e70bf00a6ff30d1d
Pablo Kaza <kazalukianpablo@gmail.com>
2026-04-30 00:28:29 -0300

    push

--- src/app/pages/my-week/success/success.ts
+++ src/app/pages/my-week/success/success.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BtnComponent } from '../../../shared/components/ui/btn/btn';
 import { WaveLogoTextComponent } from '../../../shared/components/ui/logos/wave-logo-text/wave-logo-text';
 import { WeeklyTrackings } from '../../../shared/components/widgets/users/weekly-trackings/weekly-trackings';
+import { TrackingListState } from '../../../core/services/trackings/tracking-list.state';
 
 @Component({
     selector: 'app-success',
@@ -10,4 +11,10 @@ import { WeeklyTrackings } from '../../../shared/components/widgets/users/weekly
     templateUrl: './success.html',
     styles: ``,
 })
-export class Success {}
+export class Success implements OnInit {
+    private facade = inject(TrackingListState);
+
+    ngOnInit(): void {
+        this.facade.getStats();
+    }
+}
